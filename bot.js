c88df7836a7efc6fb66bb90bb69216985b4a584c
w2lody <44412859+w2lody@users.noreply.github.com>
2018-10-28 20:14:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,3 @@
-const Discord = require('discord.js');
-
-const Util = require('discord.js');
-
-const getYoutubeID = require('get-youtube-id');
-
-const fetchVideoInfo = require('youtube-info');
-
-const YouTube = require('simple-youtube-api');
-
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-
-const queue = new Map();
-
-const ytdl = require('ytdl-core');
-
-const fs = require('fs');
-
-const gif = require("gif-search");
-
-const client = new Discord.Client({disableEveryone: true});
-
-const prefix = "w";
-/////////////////////////
-////////////////////////
-
 client.on('message', message => {
 var prefix = "f!";
 
@@ -45,7 +19,7 @@ return;
             .addField(' » Message : ', args)
             .setColor('#ff0000')
             // m.send([${m}]);
-            m.send(${m},{embed: +bc});
+            m.send(${m},{embed: bc});
         });
     }
     } else {
